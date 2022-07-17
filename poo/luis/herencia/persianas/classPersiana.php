808023948edf7e876e46f87a8b3942d5c9917d0e
José Manuel del Pino <jmdlpino@autistici.org>
2022-07-17 13:31:51 +0100

    Practicas sobre clases, objetos y herencias.

---
+++ poo/luis/herencia/persianas/classPersiana.php
@@ -0,0 +1,41 @@
+<?php // Super Clase o clase padre hereda a las sub-clases o hijas (Veneciana y Estor)
+
+    class Persiana{ // Definimos propiedades y metodos que seran heredados por las sub-clases
+        private $strNombre;
+        private $strModelo;
+        private $strColor;
+        private $fltAncho;
+        private $fltAlto;
+        private $strMaterial;
+        private $intUnidades;
+
+        function __construct(string $nombre, string $modelo, string $color, float $ancho, float $alto, string $material, int $unidades)
+        {
+            $this->strNombre = $nombre;
+            $this->strModelo = $modelo;
+            $this->strColor = $color;
+            $this->fltAncho = $ancho;
+            $this->fltAlto = $alto;
+            $this->strMaterial = $material;
+            $this->intUnidades = $unidades;
+
+        }
+
+        public function getDatosPersiana()
+        {
+            $datos = " 
+                <h2>DATOS PERSIANA</h2>
+                Nombre: {$this->strNombre}<br>
+                Modelo: {$this->strModelo}<br>
+                Color: {$this->strColor}<br>
+                Material: {$this->strMaterial}<br>
+                Unidades: {$this->intUnidades}<br>
+                Medidas (an x al): {$this->fltAncho} x {$this->fltAlto}<br>
+            "; // Se ponen comillas dobles cuando queremos colocar las variables dentro del contenido
+            return $datos;
+        }
+    }
+
+    
+
+?>
---
+++ poo/luis/herencia/persianas/pedido.php
@@ -0,0 +1,23 @@
+<?php
+
+    require_once ("classVeneciana.php"); //Requerimos la(s) clase(s)
+    require_once ("classEstor.php");
+
+    //Creamos los objetos
+    $objVeneciana = new Veneciana("Veneciana", "50mm", "Azul", 1.50, 1.80, "Aluminio", 8); //Instanciamos la(s) clase(s) y le "seteamos" o asignamos los datos
+    $objVeneciana->setMandos("Derecha");
+    $objVeneciana->setSoportes(3);
+
+    echo $objVeneciana->getDatosPersiana();
+    echo "Mandos: ".$objVeneciana->getMandos()."<br>";
+    echo "Soportes: ".$objVeneciana->getSoportes();
+        
+    $objEstor = new Estor("Estor", "Screen", "Beige", 1.10, 2.15, "Fibra de vidrio", 3);
+    $objEstor->setGrupo(4);
+    $objEstor->setReferencia(4001);
+
+    echo $objEstor->getDatosPersiana();
+    echo "Grupo: ".$objEstor->getGrupo()."<br>";
+    echo "Referencia: ".$objEstor->getReferencia();
+
+?>
